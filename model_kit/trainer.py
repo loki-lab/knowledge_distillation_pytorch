668cee35119e58f55b8c57556ff3708be6fe401d
Loke <nguyenvanminhcuong2002@gmail.com>
2024-02-10 00:32:49 +0700

    commit

--- model_kit/trainer.py
+++ model_kit/trainer.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 
 class Trainer:
@@ -30,6 +31,8 @@ class Trainer:
 
         print(f"Train set: Average loss: {total_loss:.4f}, Accuracy: {metrics:.4f}")
 
+        return metrics, total_loss
+
     def test(self, test_loader):
         with torch.no_grad():
             self.model.eval()
@@ -49,6 +52,8 @@ class Trainer:
 
             print(f"Test set: Average loss: {total_loss:.4f}, Accuracy: {metrics:.4f}")
 
+            return metrics, total_loss
+
     def predict(self, image):
         with torch.no_grad():
             self.model.eval()
@@ -67,12 +72,49 @@ class Trainer:
         }, path)
 
     def fit(self, train_loader, test_loader, epochs):
+        best_metrics = 0.0
         for epoch in range(epochs):
             print('Epoch {}/{}'.format(epoch + 1, epochs))
-            self.train(train_loader)
-            self.test(test_loader)
+            acc, loss = self.train(train_loader)
+            val_acc, val_loss = self.test(test_loader)
+
+            if val_acc > best_metrics:
+                best_metrics = acc
+                self.save_checkpoint("./checkpoints/best_weight", epoch, val_loss, val_acc)
+
+            self.save_checkpoint("./checkpoints/latest_weight", epoch, loss, acc)
 
 
 class KnowledgeDistillationTrainer(Trainer):
-    def __init__(self, teacher_model, student_model, criterion, optimizer, device):
-        super().__init__(model=student_model, criterion=criterion, optimizer=optimizer, device=device)
+    def __init__(self, teacher_model, student_model, criterion, optimizer, device, metrics, t, alpha):
+        super().__init__(model=student_model, criterion=criterion, optimizer=optimizer, device=device, metrics=metrics)
+        self.teacher_model = teacher_model
+        self.t = t
+        self.alpha = alpha
+
+    def train(self, train_loader):
+        self.model.train()
+        self.teacher_model.eval()
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            labels_distill = self.teacher_model(inputs)
+            outputs = self.model(inputs)
+            distill_loss = (self.criterion(F.log_softmax(outputs / self.t, dim=1),
+                                           F.softmax(labels_distill / self.t, dim=1)) * (self.alpha * self.t * self.t) +
+                            F.cross_entropy(outputs, labels) * (1. - self.alpha))
+
+            running_loss += distill_loss.item()
+            self.optimizer.zero_grad()
+            distill_loss.backward()
+            self.optimizer.step()
+
+        total_loss = running_loss / len(train_loader.dataset)
+
+        student_metrics = self.metrics(self.model, train_loader, self.device)
+        teacher_metrics = self.metrics(self.teacher_model, train_loader, self.device)
+
+        print(f"Distill set: "
+              f"Distill loss: {total_loss:.4f}, "
+              f"Teacher Accuracy: {teacher_metrics:.4f}, "
+              f"Student Accuracy: {student_metrics:.4f}")
