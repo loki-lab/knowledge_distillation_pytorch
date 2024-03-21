e80c60f71b81161becd5e35b879e1f4777585bb5
Loke <nguyenvanminhcuong2002@gmail.com>
2024-03-21 13:43:01 +0700

    onnx

--- export_onnx_model.py
+++ export_onnx_model.py
@@ -1,10 +1,10 @@
 import torch
 from model_kit.models import VGG11
 
-path = "./checkpoints/.."
-weights = torch.load(path)
+weight = torch.load("checkpoints/teacher_model/best_weight.pt")
 
-torch_model = VGG11.load_state_dict(weights)
+torch_model = VGG11()
+torch_model.load_state_dict(weight["model_state_dict"])
 
 torch_input = torch.randn(1, 3, 224, 224)
 onnx_model = torch.onnx.dynamo_export(torch_model, torch_input)
